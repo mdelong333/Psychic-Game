095fee59848ae110a987c65433aaed3719cea57b
murnda <mdelong333@gmail.com>
2019-09-08 23:01:45 -0400

    updated js to push userGuess into array, works at first but eventually starts giving me error that .push isn't a function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
 
-    //Create variables for wins, losses, guessed letters, remaining guesse, and array for computer choices
+//Create variables for wins, losses, guessed letters, remaining guesse, and array for computer choices
     var compOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
     var wins = 0;
     var losses = 0;
     var remainingGuess = 9;
-    var previousGuess = 0;
+    var previousGuess = [];
 
     //Create variables that reference html elements to display text
     var winsText = document.getElementById("win-txt");
@@ -14,10 +14,16 @@
     var previousGuessText = document.getElementById("prev-guess-txt");
 
     //function runs when player presses a key
-    document.onkeyup = function(event) {
+    document.onkeydown = function(event) {
+
+        
         //Ties event key to user guess and logs the guess to the console
         var userGuess = event.key;
         console.log(userGuess);
+
+        //Pushes userGuess into previousGuess array
+        var guesses = previousGuess.push(" " + userGuess);
+        
 
         //Computer chooses random letter from the array and logs it to console
         var compChoice = compOptions[Math.floor(Math.random() * compOptions.length)];
@@ -31,7 +37,7 @@
                 remainingGuess--;
                 //If remaining guesses fall to 0 losses will increase by one and game will reset
                 if (remainingGuess === 0) {
-                    losses++; remainingGuess = 9; previousGuess = 0;
+                    losses++; remainingGuess = 9; previousGuess = [];
                 }
 
                 
